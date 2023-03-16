da9eb0ec4b5cf5ac4ba5e454ea07f8cd9bc6ec99
Indra211 <indrasena0018@gmail.com>
2023-03-16 13:21:49 +0530

    Reunion-Backend

--- social_media/urls.py
+++ social_media/urls.py
@@ -2,8 +2,6 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,7 +12,5 @@ urlpatterns = [
     path("api/",include("api.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
