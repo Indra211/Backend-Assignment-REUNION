e07f4dedc924f8ee280650dde7ae3061caf123cd
Indra211 <indrasena0018@gmail.com>
2023-03-17 07:35:46 +0530

    comiit

--- social_media/urls.py
+++ social_media/urls.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",index,name="home page")
+    path("",index,name="home page"),
     path("api/",include("api.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
